cce6bc84759ea5719c00a4c95211f4cfe92df8ef
Kaustuv Bhattacharya <kbhattacharya.kaustuv@gmail.com>
2025-05-07 20:19:46 +0530

    RAG.py

--- RAG.py
+++ RAG.py
@@ -15,9 +15,9 @@
 # In[4]:
 
 # START OF SQLITE PATCH
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+#__import__('pysqlite3')
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # END OF SQLITE PATCH
 
 # Standard Libraries
